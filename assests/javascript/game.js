456225472aa8ab2e1df28367de868116bd2a6773
patrickj188@gmail.com <Patrickj188@gmail.com>
2018-12-19 17:15:27 -0600

    v1

---
+++ assests/javascript/game.js
@@ -0,0 +1,15 @@
+//what needs to happen--
+// 4 fighters & 4 bosses
+// user will pick one fighter 
+//computer will randomly pick one boss
+//fight and boss will move to fighting section 
+//user will click attack button 
+//random number will be picked for user attack 
+//computer will pick random number to attack user
+//if user defeats computer user wins - audio plays
+//computer defeats user computer winns - audio plays 
+//click button to reset characters but keep win count 
+
+var solaire = "../images/SolaireofAstor.png"
+
+
