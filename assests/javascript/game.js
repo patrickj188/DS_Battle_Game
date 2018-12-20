901b3e6686ef8d6a59e897726b0796b682b0c6ee
patrickj188@gmail.com <Patrickj188@gmail.com>
2018-12-20 11:08:17 -0600

    v3

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -17,55 +17,81 @@ var computerChoice;
 var userChoice;
 var userAttackValue;
 var computerAttackValue;
-var fighter {
-    solaire {
+var fighter = {
+    solaire: {
         name: "Solaire of Astora",
-        hp: "200",
-        weapon: "Sunlight Straight Sword"
-        covenant: "Warrior of Sunlight"
-    }
-    Ashen {
+        hp: 200,
+        weapon: "Sunlight Straight Sword",
+        covenant: "Warrior of Sunlight",
+    },
+    ashen: {
         name: "Ashen One",
-        hp: "180",
+        hp: 180,
         weapon: "Straight Sword",
         covenant: "Blue Sentinels",
-    }
-
-    onion {
+    },
+    onion: {
         name: "Siegmeyer of Catarina",
-        hp: "218",
+        hp: 218,
         weapon: "Catarina Set",
         covenant: "Onion Knights",
-
-    }
-
-    knight {
+    },
+    knight: {
         name: "Silver Knight",
-        hp: "225",
+        hp: 225,
         weapon: "Black night Sword",
-        covenant: "Silver Knights"
+        covenant: "Silver Knights",
     }
-
 };
 
-$(document).ready({
+var bosses = {
+    manus:{
+        name: "Manus, Father of the Abyss",
+        hp: 700,
+        attack: "Abyss Rain",
+    },
+    dragon:{
+        name:"Gaping Dragon",
+        hp: 400,
+        attack: "Acid Throw-Up",
 
-function reset() {
-    
-}
+    },
+    demon: {
+        name: "Capra Demon",
+        hp: 300,
+        attack: "Double Machete Swing",
+    },
+    Artorias: {
+        name: "Artorias the Abysswalke",
+        hp: 300,
+        attack: "Wrath of the Abyss",
+    }
+};
 
+$( document ).ready(function(){
 
+    initialize()
+    function setup(){
+        winCount=0;
+        lossCounter=0;
+    }
 
-
-))
-    
-
-
-
-
-
+$(".fighter-img").on("click", function (){
 
 })
 
 
 
+$(".boss-img").on("click", function(){
+
+})
+
+
+
+
+
+
+
+
+
+};
