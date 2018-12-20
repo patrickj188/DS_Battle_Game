6a51350478f18f7b33f00e5f7897cc5450a56805
patrickj188@gmail.com <Patrickj188@gmail.com>
2018-12-19 18:16:34 -0600

    v2

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -10,6 +10,62 @@
 //computer defeats user computer winns - audio plays 
 //click button to reset characters but keep win count 
 
-var solaire = "../images/SolaireofAstor.png"
+var winCount = 0;
+var lossCount = 0;
+var fighterPicked;
+var computerChoice;
+var userChoice;
+var userAttackValue;
+var computerAttackValue;
+var fighter {
+    solaire {
+        name: "Solaire of Astora",
+        hp: "200",
+        weapon: "Sunlight Straight Sword"
+        covenant: "Warrior of Sunlight"
+    }
+    Ashen {
+        name: "Ashen One",
+        hp: "180",
+        weapon: "Straight Sword",
+        covenant: "Blue Sentinels",
+    }
+
+    onion {
+        name: "Siegmeyer of Catarina",
+        hp: "218",
+        weapon: "Catarina Set",
+        covenant: "Onion Knights",
+
+    }
+
+    knight {
+        name: "Silver Knight",
+        hp: "225",
+        weapon: "Black night Sword",
+        covenant: "Silver Knights"
+    }
+
+};
+
+$(document).ready({
+
+function reset() {
+    
+}
+
+
+
+
+))
+    
+
+
+
+
+
+
+})
+
 
 
